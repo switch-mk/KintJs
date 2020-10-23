538806475198a2441051d477c34da94e2425a5fa
KIRIKAE Motoki <switch1976@gmail.com>
2020-10-24 08:58:07 +0900

    Update samp.js

--- samp.js
+++ samp.js
@@ -3,7 +3,7 @@
   kv.events.record.mounted.push(function(state){
     var targetField = '質問A'; //表示非表示を切り替えるフィールドのフィールドコード
     var targetField2 = 'リンクA'; //表示非表示を切り替えるフィールドのフィールドコード
-    alert(123456789);
-    if(state.record[targetField].value >='2' ) kv.detail.getElementByCode(targetField).style.display = 'none';
+   
+    if(state.record[targetField].value >='2' )  xalert(123456789);
   });
 })();
