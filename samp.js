31635de5c7761cbb010479add2a64d9a533e255b
KIRIKAE Motoki <switch1976@gmail.com>
2020-10-24 08:40:58 +0900

    Update and rename samp to samp.js

--- samp.js
+++ samp.js
@@ -2,6 +2,6 @@
   "use strict";
   kv.events.record.mounted.push(function(state){
     var targetField = '文字列__1行_'; //表示非表示を切り替えるフィールドのフィールドコード
-    if(!state.record[targetField].value) kv.detail.getElementByCode(targetField).style.display = 'none';
+    if(!state.record[targetField].value >=2 ) kv.detail.getElementByCode(targetField).style.display = 'none';
   });
 })();
