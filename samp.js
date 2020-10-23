c8319d3a8328a681d598c26261ad41be3b8e2bd7
KIRIKAE Motoki <switch1976@gmail.com>
2020-10-24 08:56:31 +0900

    Update samp.js

--- samp.js
+++ samp.js
@@ -4,6 +4,6 @@
     var targetField = '質問A'; //表示非表示を切り替えるフィールドのフィールドコード
     var targetField2 = 'リンクA'; //表示非表示を切り替えるフィールドのフィールドコード
     alert(123456789);
-    if(state.record[targetField].value >=2 ) kv.detail.getElementByCode(targetField).style.display = 'none';
+    if(state.record[targetField].value >='2' ) kv.detail.getElementByCode(targetField).style.display = 'none';
   });
 })();
