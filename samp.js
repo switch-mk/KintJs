03e1b1d7d39c033177f889f661e2d2090d174686
KIRIKAE Motoki <switch1976@gmail.com>
2020-10-24 10:01:18 +0900

    Update samp.js

--- samp.js
+++ samp.js
@@ -1,20 +1,9 @@
-
-
-
 (function() {
   "use strict";
-  kv.events.records.mounted = [function(state){ //ボタン非表示
-    state.records.forEach(function(record, index){
-      if(record['合計点'].value <= '2'){ //条件式 (例: record['スイッチ'].value === 'off')
-        document.querySelectorAll('.kv-table-view-content tr')[index+1].querySelector('td a').style.display = 'none';
-      }
-    });
-  }];
-  kv.events.record.mounted = [function(state){ //詳細ページ非表
-    if(state.record['合計点'].value <= '5'){ //条件式 (例: state.record['スイッチ'].value === 'off')
-      location.href = location.href.split('/detail')[0];
-      location.reload();
-    }
+  kv.events.record.mounted = [function(state){ 
+    var targetField = '合計点'; //表示非表示を切り替えるフィールドのフィールドコード
+    var targetField2 = 'リンクA'; //表示非表示を切り替えるフィールドのフィールドコード
+    if(state.record[targetField].value <= '5' ) kv.detail.getElementByCode(targetField2).style.display = 'none';
   }];
   kv.events.records.mounted = [function (state) {
     console.log(state)
