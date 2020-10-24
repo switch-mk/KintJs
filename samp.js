ff0a913b6858dfc384b7de6ab9408fcbe2e33472
KIRIKAE Motoki <switch1976@gmail.com>
2020-10-24 09:38:01 +0900

    Update samp.js

--- samp.js
+++ samp.js
@@ -1,11 +1,13 @@
-(function() {
-  "use strict";
-  kv.events.record.mounted.push(function(state){
-    var targetField = 'Q1'; //表示非表示を切り替えるフィールドのフィールドコード
-    var targetField2 = 'リンクA'; //表示非表示を切り替えるフィールドのフィールドコード
-    
-    xalert(1234);
-   
-    if(state.record[targetField].value >='2' ) kv.detail.getElementByCode(targetField).style.display = 'none';
-  });
-})();
+ (function() {
+    "use strict";
+
+    kv.events.record.mounted = [function(state){ //詳細ページ非表示
+        var targetField = 'Q1'; //表示非表示を切り替えるフィールドのフィールドコード
+        var targetField2 = 'リンクA'; //表示非表示を切り替えるフィールドのフィールドコード
+        
+        xalert(1234);
+       
+        if(state.record[targetField].value >='2' ) kv.detail.getElementByCode(targetField).style.display = 'none';
+    }];
+
+  })();
