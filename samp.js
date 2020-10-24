2ac7258bde776b278ca9e877bb1f9ab2bf4d98dd
KIRIKAE Motoki <switch1976@gmail.com>
2020-10-24 09:08:20 +0900

    Update samp.js

--- samp.js
+++ samp.js
@@ -5,6 +5,7 @@
     var targetField2 = 'リンクA'; //表示非表示を切り替えるフィールドのフィールドコード
     xalert(state.record[targetField].value);
     xalert(1234);
+    xalert(state.record[targetField2].value);
     if(state.record[targetField].value >='2' )  xalert(123);
   });
 })();
